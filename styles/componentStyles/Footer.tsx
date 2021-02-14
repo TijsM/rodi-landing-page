5917bbf7cd6c8a13ab12db0f7edac562605ea648
Tijs Martens <tijs@bothrs.com>
2021-02-14 18:24:42 +0100

    fix v-alignment

--- styles/componentStyles/Footer.tsx
+++ styles/componentStyles/Footer.tsx
@@ -4,7 +4,7 @@ export const Container = styled.footer`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-
+justify-items: center;
   @media only screen and (min-width: 768px) {
     align-items: center;
     flex-direction: row;
@@ -19,6 +19,7 @@ export const DownloadButtons = styled.div`
   gap: 12px;
 
   @media only screen and (min-width: 768px) {
+    margin-top: 0px;
     justify-content: flex-end;
   }
 `;
