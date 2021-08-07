8c5a9d3fc63dafcb053344b20da39cbc8f3f6433
Tijs Martens <tijs@bothrs.com>
2021-08-07 12:36:19 +0200

    remove logs and comments

--- hooks/useStravaAuth.tsx
+++ hooks/useStravaAuth.tsx
@@ -1,17 +1,16 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
+/**
+ * redirect to the app after authentication on strava app
+ */
 export default function useStravaAuth() {
   const router = useRouter();
 
-  // redirect to the app after authentication on strava app
   useEffect(() => {
     const code = router.query?.code;
-    console.log(code)
     if (code) {
-      console.log("here");
-      //@ts-ignore
-      window.location = "rodi://stravaAuthSuccess?code=" + code;
+      router.push("rodi://stravaAuthSuccess?code=" + code);
     }
   }, [router.query?.code]);
 }
