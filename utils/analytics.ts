4bede062d3f5fa1c4836cdd3204c7c8844b834c3
Tijs Martens <tijs@bothrs.com>
2021-12-18 12:26:35 +0100

    feat: prevent local analytics

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -3,26 +3,29 @@ import ReactGA from "react-ga";
 export type AnalyticsData = {
   category: "header" | "download section" | "Sign up popup" | "footer section";
   event: {
-    type: "click" | "submit" | "focus" ;
+    type: "click" | "submit" | "focus";
     name: string;
   };
 };
 
+const isDev = process.env.NODE_ENV === "development";
+
 export const initGA = () => {
-  ReactGA.initialize("UA-148987890-5");
+  !isDev && ReactGA.initialize("UA-148987890-5");
 };
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  !isDev && ReactGA.set({ page: window.location.pathname });
+  !isDev && ReactGA.pageview(window.location.pathname);
 };
 export const logEvent = (data: AnalyticsData) => {
-  ReactGA.event({
-    category: data.category,
-    action: data.event.type + " - " + data.event.name,
-  });
+  !isDev &&
+    ReactGA.event({
+      category: data.category,
+      action: data.event.type + " - " + data.event.name,
+    });
 };
 export const logException = (description = "", fatal = false) => {
-  if (description) {
+  if (description && !isDev) {
     ReactGA.exception({ description, fatal });
   }
 };
