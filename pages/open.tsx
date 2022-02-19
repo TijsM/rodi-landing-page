482e97cfc7ccfd64a0404236b1aef59ef1f51932
Tijs Martens <tijs@bothrs.com>
2022-02-19 18:03:32 +0100

    feat: add test url

--- pages/open.tsx
+++ pages/open.tsx
@@ -1,12 +1,22 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Download from "../components/Download";
+import Features from "../components/Features";
 
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 import SeoHeaders from "../components/SeoHeaders";
 import { PageLayout } from "../styles/Layouts";
+import { useLogPageView } from "../utils/analytics";
 
 export default function Open() {
   const router = useRouter();
+  const [startTime, setStartTime] = useState<Date | undefined>();
+  useLogPageView();
+
+  useEffect(() => {
+    setStartTime(new Date());
+  }, []);
 
   useEffect(() => {
     router.push("rodi://openCoordinate/?lat=51.0543422&lon=3.7174243");
@@ -19,7 +29,16 @@ export default function Open() {
           "Download Rodi on iOS. Rodi is a free app that gives you more insights in your bike rides"
         }
       />
-      {/* <Header title={blog.title} intro={blog.intro} /> */}
+
+      <a href="rodi://openCoordinate/?lat=51.0543422&lon=3.7174243">
+        My Facebook Group
+      </a>
+
+      <Header />
+
+      <Download />
+      <Features startTime={startTime} />
+
       <PageLayout></PageLayout>
       <Footer />
     </>
