9bdbf30384805e1f4e5e18caf3251508a04b3885
Tijs Martens <tijs.martens.tijs@gmail.com>
2024-02-14 15:39:22 +0100

    fix: disable stories

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <Screen />
         <Download />
         <Journey />
-        <Stories />
+        {/* <Stories /> */}
         <Footer />
       </ScreenWidth>
     </>
